0b905bd483ca70af9f6f03dd40b2f304e36bd148
Bart Kamphorst <bart@kamphorst.com>
2012-10-21 23:03:21 +0200

    Refactored mail specs using contexts and exit_with_code matcher.

--- spec/mail_spec.rb
+++ spec/mail_spec.rb
@@ -1,10 +1,9 @@
-describe "cron report" do
+describe "A RubyCronJob" do
   
   require 'RubyCron'
   
   before(:each) do
-    
-    smtpsettings = { 	
+    @smtpsettings = { 	
       :address              => "smtp.gmail.com",
       :port                 => 587,
       :domain               => 'your.host.name',
@@ -13,29 +12,56 @@ describe "cron report" do
     	:authentication       => 'plain',
     	:enable_starttls_auto => true  
     }
-    
-    @rcj = RubyCron::RubyCronJob.new(
+    @rcjsettings = {
       :author         => 'John Doe',
       :name           => 'test',
       :mailto         => 'john@doe.com',
       :mailon         => :all,
       :exiton         => :none,
       :verbose        => false,
-      :smtpsettings   => smtpsettings )
+    }
   end
   
-  it "should load a ERB template" do
-    @rcj.template.should be
-    File.basename(@rcj.template).should == "report.erb"
-    @rcj.template = "my_template.erb"
-    File.basename(@rcj.template).should == "my_template.erb"
+  context "with complete smtp settings" do
+    
+    before(:each) do
+      @rcjsettings[:smtpsettings] = @smtpsettings
+      @rcj = RubyCron::RubyCronJob.new(@rcjsettings)
+    end
+  
+    it "should load a ERB template" do
+      @rcj.template.should be
+      File.basename(@rcj.template).should == "report.erb"
+      @rcj.template = "my_template.erb"
+      File.basename(@rcj.template).should == "my_template.erb"
+    end
+    
+    it "should have a Hash of smtp settings" do
+      @rcj.smtpsettings.should be_a Hash
+    end
+  
+    it "should have proper smtp settings" do
+      @rcj.smtpsettings[:address].should   == "smtp.gmail.com"
+      @rcj.smtpsettings[:port].should      == 587
+      @rcj.smtpsettings[:user_name].should == "<username>"
+      @rcj.smtpsettings[:password].should  == "<password>"
+    end
   end
   
-  it "should have proper smtp settings" do
-    @rcj.smtpsettings[:address].should   == "smtp.gmail.com"
-    @rcj.smtpsettings[:port].should      == 587
-    @rcj.smtpsettings[:user_name].should == "<username>"
-    @rcj.smtpsettings[:password].should  == "<password>"
+  context "with incomplete smtp settings" do
+    
+    it "should exit with code 1 if no address is specified" do
+      @smtpsettings.delete(:address)
+      @rcjsettings[:smtpsettings] = @smtpsettings
+      lambda { RubyCron::RubyCronJob.new(@rcjsettings) }.should exit_with_code(1)
+    end
+    
+    it "should exit with code 1 if no port is specified" do
+      @smtpsettings.delete(:port)
+      @rcjsettings[:smtpsettings] = @smtpsettings
+      lambda { RubyCron::RubyCronJob.new(@rcjsettings) }.should exit_with_code(1)
+    end
+      
   end
   
   after(:each) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,5 @@
 ENV['RSPEC'] = "true"
+
+# Require custom RSpec matchers
+Dir[File.dirname(__FILE__) + "/support/matchers/*.rb"].each {|f| require f}
+
