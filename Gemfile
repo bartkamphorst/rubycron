a4d4c39dfd5ab7b9edddea7f9fe0700d37eee778
Bart Kamphorst <bart@kamphorst.com>
2012-10-24 19:15:14 +0200

    Added simplecov dependency.

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ gem "mail"
 
 group :test do
   gem "rspec", "~> 2.0"
+  gem "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,21 @@
 ENV['RSPEC'] = "true"
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/support/"
+end
+
+require 'RubyCron'
+
+# Monkeypatch to bypass the check for a 
+# local smtp server. 
+module RubyCron
+  class RubyCronJob
+    def smtp_connection? ; true ; end
+  end
+end
+
 require 'mail'
 # Using Mail's TestMailer to test delivery
 Mail.defaults do
